bf63594d01d25088c26da18476bf7c7b1a01ac37
Maslyna <sanci324@gmail.com>
2023-04-11 19:35:29 +0200

    AWS S3 added

---
+++ src/main/java/com/provedcode/aws/service/S3Service.java
@@ -0,0 +1,68 @@
+package com.provedcode.aws.service;
+
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.*;
+import com.amazonaws.util.IOUtils;
+import com.provedcode.config.AWSProperties;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class S3Service implements FileService {
+    AWSProperties awsProperties;
+    AmazonS3 s3;
+    @Override
+    public String saveFile(MultipartFile file) {
+        String originalFilename = file.getOriginalFilename();
+        try {
+            File file1 = convertMultiPartToFile(file);
+            PutObjectResult objectResult = s3.putObject(awsProperties.bucket(), originalFilename, file1);
+            return objectResult.getContentMd5();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public byte[] downloadFile(String filename) {
+        S3Object object = s3.getObject(awsProperties.bucket(), filename);
+        S3ObjectInputStream objectContent = object.getObjectContent();
+        try {
+            return IOUtils.toByteArray(objectContent);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String deleteFile(String filename) {
+        s3.deleteObject(awsProperties.bucket(), filename);
+        return "file deleted";
+    }
+
+    @Override
+    public List<String> listAllFiles() {
+        ListObjectsV2Result listObjectsV2Result = s3.listObjectsV2(awsProperties.bucket());
+        return listObjectsV2Result.getObjectSummaries().stream().map(S3ObjectSummary::getKey).collect(Collectors.toList());
+    }
+
+    private File convertMultiPartToFile(MultipartFile file) throws IOException {
+        File convFile = new File(file.getOriginalFilename());
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convFile;
+    }
+
+}
